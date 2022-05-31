65bc9d3ef730152e308861be435378e36726139b
Igor <igor_kichkar@mail.ru>
2022-05-31 22:32:41 +0200

    Added backend filter for hydrants

--- client/hydrant_client/src/components/UI/MyInput.js
+++ client/hydrant_client/src/components/UI/MyInput.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import {useEffect} from "react";
 import axios from "axios";
 
-const MyInput = ({loadingAfterFilter, setLoadingAfterFilter, listHydrants, setListHydrants, setLoading}) => {
+const gomelskaya = [
+    {title: "Брагинский", value: "Braginskij"},
+    {title: "Буда - Кошелёвский", value: "Buda_Koshelyovskij"},
+    {title: "Ветковский", value: "Vetkovskij"},
+    {title: "Гомель", value: "Gomel"},
+    {title: "Гомельский", value: "Gomelskij"},
+    {title: "Добрушский", value: "Dobrushskij"},
+    {title: "Ельский", value: "Elskij"},
+    {title: "Житковичский", value: "Zhitkovichskij"},
+    {title: "Жлобинский", value: "Zhlobinskij"},
+    {title: "Калинковичский", value: "Kalinkovichskij"},
+    {title: "Лельчицкий", value: "Lelchickij"},
+    {title: "Лоевский", value: "Loevskij"},
+    {title: "Мозырский", value: "Mozyrskij"},
+    {title: "Наровлянский", value: "Narovlyanskij"},
+    {title: "Октябрьский", value: "Oktyabrskij"},
+    {title: "Петриковский", value: "Petrikovskij"},
+    {title: "Речицкий", value: "Rechickij"},
+    {title: "Рогачёвский", value: "Rogachyovskij"},
+    {title: "Светлогорский", value: "Svetlogorskij"},
+    {title: "Хойникский", value: "Hojnikskij"},
+    {title: "Чечерский", value: "CHecherskij"},
+]
 
-    function selectRegion(value) {
-        setLoadingAfterFilter({...loadingAfterFilter, region: value})
+const MyInput = ({loadingAfterFilter, setLoadingAfterFilter, listHydrants, setListHydrants, setLoading}) => {
+    function onSubmit(e) {
+        e.preventDefault();
+        updateHydrants();
     }
 
     function updateHydrants() {
         setLoading(true);
         (async () => {
-            const response = await axios.get("hydrants/", { params: { answer: 102 } });
+            const response = await axios.get("hydrants/", {params: {answer: 102}});
             if (response.status === 200) {
                 setListHydrants(response.data);
             }
@@ -19,53 +41,80 @@ const MyInput = ({loadingAfterFilter, setLoadingAfterFilter, listHydrants, setLi
         })();
     }
 
-
-    function selectArea() {
-        const Gomelskaja = [
-            'Лоевский',
-            'Речицкий',
-            'Кормянский',
-            'Наровлянский',
-            'Гомельский',
-            'Брагинский'
-        ]
-        if (loadingAfterFilter.region === 'Гомельская') {
-            return Gomelskaja
-        }
-        return []
+    function selectRegion(value) {
+        setLoadingAfterFilter({...loadingAfterFilter, region: value})
     }
 
+
     return (
+        // <div>
+        //     <label htmlFor="region">Область:</label>
+        //     <input list="regions" id="region" onChange={e => selectRegion(e.target.value)}/>
+        //     <datalist id="regions">
+        //         <option>Брестская</option>
+        //         <option>Витебская</option>
+        //         <option>Гомельская</option>
+        //         <option>Гродненская</option>
+        //         <option>Могилевскя</option>
+        //         <option>Минская</option>
+        //     </datalist>
+        //
+        //     <label htmlFor="area">Район:</label>
+        //     <input list="areas" id="area" disabled={!loadingAfterFilter.region}/>
+        //     <datalist id="areas">
+        //         {selectArea().map((item, index) =>
+        //             <option key={index}>{item}</option>
+        //         )}
+        //     </datalist>
+        //
+        //     <label htmlFor="locality">Населенный пункт:</label>
+        //     <input id="locality"/>
+        //
+        //     <label htmlFor="street">Улица:</label>
+        //     <input id="street"/>
+        //
+        //     <button onClick={() => {
+        //         updateHydrants()
+        //     }}>Фильтровать
+        //     </button>
+        //
+        // </div>
         <div>
-
-
-                <label htmlFor="region">Область:</label>
-                <input list="regions" id="region" onChange={e => selectRegion(e.target.value)}/>
-                <datalist id="regions">
-                    <option>Брестская</option>
-                    <option>Витебская</option>
-                    <option>Гомельская</option>
-                    <option>Гродненская</option>
-                    <option>Могилевскя</option>
-                    <option>Минская</option>
-                </datalist>
-
-                <label htmlFor="area">Район:</label>
-                <input list="areas" id="area" disabled={!loadingAfterFilter.region}/>
-                <datalist id="areas">
-                    {selectArea().map((item, index) =>
-                        <option key={index}>{item}</option>
-                    )}
-                </datalist>
-
-                <label htmlFor="locality">Населенный пункт:</label>
-                <input id="locality"/>
-
-                <label htmlFor="street">Улица:</label>
-                <input id="street"/>
-
-                <button onClick={()=> {updateHydrants()}}>Фильтровать</button>
-
+            <form onSubmit={onSubmit}>
+                <div className="inputFilter">
+                    <p>Район</p>
+                    <select defaultValue="" name="select">
+                        <option value=""></option>
+                        {gomelskaya.map((area) =>
+                            <option key={area.value} value={area.value}>{area.title}</option>
+                        )}
+                    </select>
+                </div>
+                <div className="inputFilter">
+                    <p>Населенный пункт</p>
+                    <input/>
+                </div>
+                <div className="inputFilter">
+                    <p>Улица</p>
+                    <input/>
+                </div>
+                <div className="inputFilter">
+                    <p>Принадлежность</p>
+                    <input/>
+                </div>
+                <div className="inputFilter">
+                    <p>Исправность</p>
+                    <select defaultValue="all" name="select">
+                        <option value="all">Все</option>
+                        <option value="serviceable">Исправные</option>
+                        <option value="faulty">Неисправные</option>
+                    </select>
+                </div>
+                <br/>
+                <br/>
+                <input type="submit" value="Получит excel отчет"/>
+                <input type="submit" value="Загрузить на страницу"/>
+            </form>
         </div>
     );
 };
--- server/hydrant_server/main_app/views.py
+++ server/hydrant_server/main_app/views.py
@@ -11,8 +11,28 @@ class HydrantViewSet(viewsets.ModelViewSet):
     serializer_class = HydrantSerializer
 
     def get_queryset(self):
-        print(self.request.query_params)
-        # print(self.request.query_params.get('parameter1'))
-        queryset = Hydrant.objects.all()
+        street = self.request.query_params.get('street') if self.request.query_params.get('street') else ''
+        area = self.request.query_params.get('area') if self.request.query_params.get('area') else ''
+        locality = self.request.query_params.get('locality') if self.request.query_params.get('locality') else ''
+        belonging = self.request.query_params.get('belonging') if self.request.query_params.get('belonging') else ''
+        serviceability = self.request.query_params.get('serviceability')
+        if serviceability == 'true' or serviceability == 'false':
+            if self.request.query_params.get('serviceability') == "true":
+                serviceability = True
+            elif self.request.query_params.get('serviceability') == "false":
+                serviceability = False
+            queryset = Hydrant.objects.filter(address_district__icontains=area,
+                                              address_detail__icontains=street,
+                                              address_town__icontains=locality,
+                                              belonging__icontains=belonging,
+                                              serviceable=serviceability
+                                              )
+            return queryset
+
+        queryset = Hydrant.objects.filter(address_district__icontains=area,
+                                          address_detail__icontains=street,
+                                          address_town__icontains=locality,
+                                          belonging__icontains=belonging,
+                                          )
 
         return queryset
